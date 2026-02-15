4b9fe85ff0a2c09d4aaa3aa2071b11da38df18b4
Alexander Nollet <alexanderacj13@gmail.com>
2026-02-15 16:36:39 +0100

    Shared test suite works like intended

--- apps/frontend/app/app.vue
+++ apps/frontend/app/app.vue
@@ -1,26 +1,6 @@
-<script setup lang="ts">
-const config = useRuntimeConfig()
-
-const { data, status, error, refresh } = await useFetch("/", {
-  baseURL: config.public.apiBase // Uses the APIBase configured in the Config.
-})
-</script>
-
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    
-    <div v-if="data">
-      {{ data }}
-    </div>
-
-    <div v-else-if="error" style="color: red; border: 1px solid red; padding: 10px;">
-      <p><strong>Error Detected:</strong></p>
-      <pre>{{ error }}</pre>
-    </div>
-
-    <div v-else>
-      Loading...
-    </div>
+    <NuxtPage />
   </div>
 </template>
