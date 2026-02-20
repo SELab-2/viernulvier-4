dce79466c325cb1fe5213805ca8cd38031721434
Dominik <dominik.nowak2003@gmail.com>
2026-02-20 12:54:13 +0100

    wrond import fix

--- backend/src/events/events.module.ts
+++ backend/src/events/events.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { EventsService } from "./events.service";
 import { EventsController } from "./events.controller";
 import { EventDatabaseService } from "src/database/db.event.service";
+import { DbModule } from "../database/db.module";
 
 @Module({
   providers: [EventsService],
   controllers: [EventsController],
-  imports: [EventDatabaseService],
+  imports: [DbModule],
 })
 export class EventsModule {}
--- backend/src/productions/productions.module.ts
+++ backend/src/productions/productions.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { ProductionsService } from "./productions.service";
 import { ProductionsController } from "./productions.controller";
 import { ProductionDatabaseService } from "src/database/db.production.service";
+import { DbModule } from "../database/db.module";
 
 @Module({
   providers: [ProductionsService],
   controllers: [ProductionsController],
-  imports: [ProductionDatabaseService],
+  imports: [DbModule],
 })
 export class ProductionsModule {}
