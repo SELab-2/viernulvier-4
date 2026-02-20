1429e8935d6e16964b7a772d88b7f430b2cdc68a
Alexander Nollet <alexanderacj13@gmail.com>
2026-02-20 13:06:57 +0100

    Fixed imports of DBMODULE

--- backend/src/productions/productions.module.ts
+++ backend/src/productions/productions.module.ts
@@ -1,11 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ProductionsService } from "./productions.service";
 import { ProductionsController } from "./productions.controller";
-import { ProductionDatabaseService } from "src/database/db.production.service";
+import { DbModule } from "src/database/db.module";
 
 @Module({
   providers: [ProductionsService],
   controllers: [ProductionsController],
-  imports: [ProductionDatabaseService],
+  imports: [DbModule], // Import DB Module here so we can use it's services.
 })
 export class ProductionsModule {}
