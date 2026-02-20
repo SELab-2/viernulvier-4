c36339a6368ec0f59f533fd3392719833adb5a40
landgeet <lander.degeeter@ugent.be>
2026-02-20 13:58:12 +0100

    import dbmodules

--- backend/src/event/event.module.ts
+++ backend/src/event/event.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { EventService } from "./event.service";
 import { EventController } from "./event.controller";
+import { DbModule } from "src/database/db.module";
 
 @Module({
   providers: [EventService],
   controllers: [EventController],
+  imports: [DbModule],
 })
 export class EventModule {}
