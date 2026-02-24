0fc733ace7cf4866c16cee3ca5c05cda1e0f5a1f
Alexander Nollet <alexanderacj13@gmail.com>
2026-02-24 11:05:47 +0100

    Blogs have been swagger documented

--- backend/src/blog/blog.controller.ts
+++ backend/src/blog/blog.controller.ts
@@ -3,44 +3,61 @@ import { BlogService } from './blog.service';
 import type { Blog, CreateBlog, UpdateBlog } from "@repo/common";
 import { BlogSchema, CreateBlogSchema, UpdateBlogSchema } from '@repo/common';
 import { ZodValidationPipe } from 'src/common/pipes/zod.validation.pipe';
+import { BlogDto, CreateBlogDto, UpdateBlogDto } from 'src/dto/dto';
+import { ApiBody, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('blog')
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
 
+  @ApiOperation({ summary: "Returns all blogs." })
+  @ApiOkResponse({ type: BlogDto, isArray: true, description: "Returned all blogs." })
   @Get()
-  async getAllBlogs(): Promise<Blog[]> {
+  async getAllBlogs(): Promise<BlogDto[]> {
     return await this.blogService.getAllBlogs();
   }
 
+  @ApiOperation({ summary: "Returns a single blog." })
+  @ApiOkResponse({ type: BlogDto, description: "Found Blog." })
   @Get(":id")
-  async getBlogById(@Param("id", ParseIntPipe) id: number): Promise<Blog> {
+  async getBlogById(@Param("id", ParseIntPipe) id: number): Promise<BlogDto> {
     return await this.blogService.getBlogById(id);
   }
 
+  @ApiOperation({ summary: "Creates a new blog." })
+  @ApiBody({ type: CreateBlogDto })
+  @ApiOkResponse({ type: BlogDto, description: "Created a new Blog." })
   @Post()
   async createBlog(
-    @Body(new ZodValidationPipe(CreateBlogSchema)) createBlog: CreateBlog
-  ): Promise<Blog> {
+    @Body(new ZodValidationPipe(CreateBlogSchema)) createBlog: CreateBlogDto
+  ): Promise<BlogDto> {
     return await this.blogService.createBlog(createBlog);
   }
 
+  @ApiOperation({ summary: "Replaces an existing blog." })
+  @ApiBody({ type: BlogDto })
+  @ApiOkResponse({ type: BlogDto, description: "Replaced Blog." })
   @Put(":id")
   async replaceBlog(
     @Param("id", ParseIntPipe) id: number,
-    @Body(new ZodValidationPipe(BlogSchema)) blog: Blog,
-  ): Promise<Blog> {
+    @Body(new ZodValidationPipe(BlogSchema)) blog: BlogDto,
+  ): Promise<BlogDto> {
     return await this.blogService.replaceBlog(id, blog);
   }
 
+  @ApiOperation({ summary: "Modifies an existing blog." })
+  @ApiBody({ type: UpdateBlogDto })
+  @ApiOkResponse({ type: BlogDto, description: "Modified Blog." })
   @Patch(":id")
   async modifyBlog(
     @Param("id", ParseIntPipe) id: number,
-    @Body(new ZodValidationPipe(UpdateBlogSchema)) blog: UpdateBlog,
-  ): Promise<Blog> {
+    @Body(new ZodValidationPipe(UpdateBlogSchema)) blog: UpdateBlogDto,
+  ): Promise<BlogDto> {
     return await this.blogService.modifyBlog(id, blog);
   }
 
+  @ApiOperation({ summary: "Delete an existing blog." })
+  @ApiOkResponse({ description: "Deleted Blog." })
   @Delete(":id")
   async deleteBlog(@Param("id", ParseIntPipe) id: number): Promise<void> {
     return await this.blogService.deleteBlog(id);
--- backend/src/dto/dto.ts
+++ backend/src/dto/dto.ts
@@ -1,4 +1,4 @@
-import { CreateEventSchema, CreateProductionSchema, EventSchema, ProductionSchema, UpdateEventSchema, UpdateProductionSchema } from "@repo/common";
+import { BlogSchema, CreateBlogSchema, CreateEventSchema, CreateProductionSchema, EventSchema, ProductionSchema, UpdateBlogSchema, UpdateEventSchema, UpdateProductionSchema } from "@repo/common";
 import { createZodDto } from "nestjs-zod";
 // This file wraps the Objects into an DTO Swagger can see.
 
@@ -11,3 +11,8 @@ export class UpdateProductionDto extends createZodDto(UpdateProductionSchema) {}
 export class EventDto extends createZodDto(EventSchema) {}
 export class CreateEventDto extends createZodDto(CreateEventSchema) {}
 export class UpdateEventDto extends createZodDto(UpdateEventSchema) {}
+
+// Blogs
+export class BlogDto extends createZodDto(BlogSchema) {}
+export class CreateBlogDto extends createZodDto(CreateBlogSchema) {}
+export class UpdateBlogDto extends createZodDto(UpdateBlogSchema) {}
