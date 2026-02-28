451d15acd9f911fe1cfcdb7821c0d0fa8870fef2
landgeet <lander.degeeter@ugent.be>
2026-02-28 15:26:19 +0100

    implement event transform function

--- common/src/csv_parser.ts
+++ common/src/csv_parser.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import csvParser from "csv-parser";
 import { ZodSchema } from "zod";
+import { CreateEventDto } from "../../backend/src/dto/dto";
 
 export class CSVParser {
   /**
@@ -19,12 +20,14 @@ export class CSVParser {
       const results: T[] = [];
 
       fs.createReadStream(filePath)
-        .pipe(csvParser({
-          mapHeaders: ({ header }) => header.trim(), // Trim whitespace from headers
-          mapValues: ({ value }) => value.trim(), // Trim whitespace from values
-        }))
+        .pipe(
+          csvParser({
+            mapHeaders: ({ header }) => header.trim(), // Trim whitespace from headers
+            mapValues: ({ value }) => value.trim(), // Trim whitespace from values
+          }),
+        )
         .on("data", (row: Record<string, string>) => {
-          try{
+          try {
             // apply the transformation function to the row and validate it against the schema
             const transformedRow: T = transform(row);
             // try to parse the transformed row with the schema, if it fails, catch the error and reject the promise
@@ -41,5 +44,30 @@ export class CSVParser {
           reject(`Error reading CSV file: ${error}`);
         });
     });
+  }
+
+  static transformEventRow(row: Record<string, string>): CreateEventDto {
+    let endTime: string | null = null;
+
+    // Check for invalid date formats and handle them accordingly
+    const invalidDates = ["0000-00-00 00:00:00", "1970-01-01 00:00:00", ""];
+    const starttimeDate = new Date(row.Starttime);
+    if (row.EndTime && !invalidDates.includes(row.EndTime)) {
+      const endTimeDate = new Date(row.EndTime);
+      if (!isNaN(endTimeDate.getTime()) && endTimeDate > starttimeDate)
+        endTime = endTimeDate.toISOString();
+    }
+
+    const productionId = Number(row.Production);
+    if (isNaN(productionId))
+      throw new Error(`Invalid production_id: ${row.Production}`);
+
+    return {
+      starttime: starttimeDate.toISOString(),
+      endtime: endTime,
+      hall: row.Hall,
+      production_id: productionId,
+      price: row.Price ? Number(row.Price) : null,
+    };
   }
 }
