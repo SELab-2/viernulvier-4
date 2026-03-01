80c2ab6612c6e78619dc3d26daa58cd9fcfc4130
Robin De Rudder <robin.derudder@ugent.be>
2026-03-01 17:36:21 +0100

    no more (own generated) error in terminal

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -114,7 +114,7 @@ async function buildApp(): Promise<INestApplication> {
     .compile();
 
   const app = moduleFixture.createNestApplication();
-  // app.useLogger(false);  remove these comments if we don't want the own generated error being shown in the terminal
+  app.useLogger(false);
   await app.init();
   return app;
 }
