0dbcd8689ebd45d8a93180eec49ff173b3f6b06a
Alexander Nollet <alexanderacj13@gmail.com>
2026-03-02 13:14:05 +0100

    Revert "Inserting now added as an extra endpoing"
    This reverts commit 68b2a6b38bdcf760828deb4b44f7b6a3ae845986.

--- backend/src/production/controllers/production.controller.ts
+++ backend/src/production/controllers/production.controller.ts
@@ -141,17 +141,6 @@ export class ProductionController {
   async createProduction(
     @Body() newProduction: CreateProductionDto,
   ): Promise<ProductionDto> {
-    return await this.productionService.createProduction(newProduction);
-  }
-
-  @ApiOperation({ summary: "Inserts a Production." })
-  @ApiBody({ type: ProductionDto })
-  @ApiOkResponse({ type: ProductionDto, description: "Production inserted." })
-  @Post("insert")
-  @UsePipes(new ZodValidationPipe(ProductionSchema))
-  async insertProduction(
-    @Body() production: ProductionDto,
-  ): Promise<ProductionDto> {
-    return await this.productionService.insertProduction(production); 
+    return this.productionService.createProduction(newProduction);
   }
 }
