de3c07c2c88ea76206ce34c4163d9b77d55539f7
Alexander Nollet <alexanderacj13@gmail.com>
2026-03-02 15:11:18 +0100

    Temp commit

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { AccountDatabaseService } from "../database/db.account.service";
+import { ApiKeyDatabaseService } from "../database/db.apiKey.service";
+import { ApiKeyDto, CreateAccountDto, PublicAccountDto } from "../dto/dto";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly accountDbService: AccountDatabaseService,
+    private readonly apiKeyDbService: ApiKeyDatabaseService,
+  ) {}
+
+  async loginAccount(
+    account: CreateAccountDto,
+  ): Promise<{ account: PublicAccountDto; apiKey: ApiKeyDto | null }> {
+    return await this.accountDbService.loginAccount(account);
+  }
+
+  async createAccount(account: CreateAccountDto): Promise<boolean> {
+    const valid: boolean = await this.accountDbService.createAccount(account);
+    if (!valid) throw new Error("Could not create account.");
+
+    const accounts: PublicAccountDto = await this.accountDbService.get()
+    const apiKey: ApiKeyDto = await this.apiKeyDbService.generateApiKey();
+    await this.accountDbService.linkAccountToKey()
+  }
+}
