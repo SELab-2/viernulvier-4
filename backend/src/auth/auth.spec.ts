fb5d491e5b2879c32a49f9b5baf6c61122da840b
Alexander Nollet <alexanderacj13@gmail.com>
2026-03-03 16:12:39 +0100

    Fixed all tests with new endpoint names

--- backend/src/auth/auth.spec.ts
+++ backend/src/auth/auth.spec.ts
@@ -21,186 +21,190 @@ describe("AuthGuards tests", () => {
 
   describe("Event endpoints authentication", () => {
     it("GET /events should work without API key", async () => {
-      await request(app.getHttpServer()).get("/event").expect(200);
+      await request(app.getHttpServer()).get("/events").expect(200);
     });
 
     it("GET /events/:id should work without API key", async () => {
-      await request(app.getHttpServer()).get("/event/1").send({}).expect(200);
+      await request(app.getHttpServer()).get("/events/1").send({}).expect(200);
     });
 
     it("POST /events should fail without API key", async () => {
-      await request(app.getHttpServer()).post("/event").send({}).expect(401);
+      await request(app.getHttpServer()).post("/events").send({}).expect(401);
     });
 
     it("PUT /events/:id should fail without API key", async () => {
-      await request(app.getHttpServer()).put("/event/1").send({}).expect(401);
+      await request(app.getHttpServer()).put("/events/1").send({}).expect(401);
     });
 
     it("DELETE /events/:id should fail without API key", async () => {
-      await request(app.getHttpServer()).delete("/event/1").expect(401);
+      await request(app.getHttpServer()).delete("/events/1").expect(401);
     });
 
     it("GET /events/:eventId/locations", async () => {
-      await request(app.getHttpServer()).get("/event/1/location").expect(200);
+      await request(app.getHttpServer()).get("/events/1/locations").expect(200);
     });
   });
 
   describe("Event - Location endpoints authentication", () => {
     it("GET /events/:eventId/locations should work without API key", async () => {
-      await request(app.getHttpServer()).get("/event/1/location").expect(200);
+      await request(app.getHttpServer()).get("/events/1/locations").expect(200);
     });
 
     it("PUT /events/:eventId/locations/:locationId should fail without API key.", async () => {
-      await request(app.getHttpServer()).put("/event/1/location/1").expect(401);
+      await request(app.getHttpServer())
+        .put("/events/1/locations/1")
+        .expect(401);
     });
 
     it("DELETE /events/:eventId/locations should fail without API key.", async () => {
       await request(app.getHttpServer())
-        .delete("/event/1/location")
+        .delete("/events/1/locations")
         .expect(401);
     });
   });
 
   describe("Production endpoints authentication", () => {
     it("GET /productions should work without API key", async () => {
-      await request(app.getHttpServer()).get("/production").expect(200);
+      await request(app.getHttpServer()).get("/productions").expect(200);
     });
 
     it("GET /productions/:id should work without API key", async () => {
       await request(app.getHttpServer())
-        .get("/production/1")
+        .get("/productions/1")
         .send({})
         .expect(200);
     });
 
     it("POST /productions should fail without API key", async () => {
       await request(app.getHttpServer())
-        .post("/production")
+        .post("/productions")
         .send({})
         .expect(401);
     });
 
     it("PUT /productions/:id should fail without API key", async () => {
       await request(app.getHttpServer())
-        .put("/production/1")
+        .put("/productions/1")
         .send({})
         .expect(401);
     });
 
     it("DELETE /productions/:id should fail without API key", async () => {
-      await request(app.getHttpServer()).delete("/production/1").expect(401);
+      await request(app.getHttpServer()).delete("/productions/1").expect(401);
     });
 
     it("PATCH /productions/:id should fail without API key", async () => {
-      await request(app.getHttpServer()).patch("/production/1").expect(401);
+      await request(app.getHttpServer()).patch("/productions/1").expect(401);
     });
   });
 
   describe("Production-blog endpoints authentication", () => {
     it("GET /productions/:id/blogs should work without API key", async () => {
-      await request(app.getHttpServer()).get("/production/1/blog").expect(200);
+      await request(app.getHttpServer())
+        .get("/productions/1/blogs")
+        .expect(200);
     });
 
     it("PUT /productions/:id/blogs/:id should fail without API key", async () => {
       await request(app.getHttpServer())
-        .put("/production/1/blog/1")
+        .put("/productions/1/blogs/1")
         .send({})
         .expect(401);
     });
 
     it("DELETE /productions/:id/blogs/:id  should fail without API key", async () => {
       await request(app.getHttpServer())
-        .delete("/production/1/blog/1")
+        .delete("/productions/1/blogs/1")
         .expect(401);
     });
   });
 
   describe("Production-tag endpoints authentication", () => {
     it("GET /productions/:id/tags should work without API key", async () => {
-      await request(app.getHttpServer()).get("/production/1/tag").expect(200);
+      await request(app.getHttpServer()).get("/productions/1/tags").expect(200);
     });
 
     it("PUT /productions/:id/tags/:id should fail without API key", async () => {
       await request(app.getHttpServer())
-        .put("/production/1/tag/1")
+        .put("/productions/1/tags/1")
         .send({})
         .expect(401);
     });
 
     it("DELETE /productions/:id/tags/:id  should fail without API key", async () => {
       await request(app.getHttpServer())
-        .delete("/production/1/tag/1")
+        .delete("/productions/1/tags/1")
         .expect(401);
     });
   });
 
   describe("Tag endpoints authentication", () => {
     it("GET /tags should work without API key", async () => {
-      await request(app.getHttpServer()).get("/tag").expect(200);
+      await request(app.getHttpServer()).get("/tags").expect(200);
     });
 
     it("POST /tags should fail without API key", async () => {
-      await request(app.getHttpServer()).post("/tag").send({}).expect(401);
+      await request(app.getHttpServer()).post("/tags").send({}).expect(401);
     });
 
     it("GET /tags/:id should work without API key", async () => {
-      await request(app.getHttpServer()).get("/tag/1").send({}).expect(200);
+      await request(app.getHttpServer()).get("/tags/1").send({}).expect(200);
     });
 
     it("DELETE /tags/:id should fail without API key", async () => {
-      await request(app.getHttpServer()).delete("/tag/1").expect(401);
+      await request(app.getHttpServer()).delete("/tags/1").expect(401);
     });
 
     it("PATCH /tags/:id should fail without API key", async () => {
-      await request(app.getHttpServer()).patch("/tag/1").expect(401);
+      await request(app.getHttpServer()).patch("/tags/1").expect(401);
     });
   });
 
   describe("Blog endpoints authentication", () => {
     it("GET /blogs should work without API key", async () => {
-      await request(app.getHttpServer()).get("/blog").expect(200);
+      await request(app.getHttpServer()).get("/blogs").expect(200);
     });
 
     it("POST /blogs should fail without API key", async () => {
-      await request(app.getHttpServer()).post("/blog").send({}).expect(401);
+      await request(app.getHttpServer()).post("/blogs").send({}).expect(401);
     });
 
     it("GET /blogs/:id should work without API key", async () => {
-      await request(app.getHttpServer()).get("/blog/1").send({}).expect(200);
+      await request(app.getHttpServer()).get("/blogs/1").send({}).expect(200);
     });
 
     it("DELETE /blogs/:id should fail without API key", async () => {
-      await request(app.getHttpServer()).delete("/blog/1").expect(401);
+      await request(app.getHttpServer()).delete("/blogs/1").expect(401);
     });
 
     it("PATCH /blogs/:id should fail without API key", async () => {
-      await request(app.getHttpServer()).patch("/blog/1").expect(401);
+      await request(app.getHttpServer()).patch("/blogs/1").expect(401);
     });
 
     it("PUT /blogs/:id should fail without API key", async () => {
-      await request(app.getHttpServer()).put("/blog/1").expect(401);
+      await request(app.getHttpServer()).put("/blogs/1").expect(401);
     });
   });
 
   describe("Location endpoints authentication", () => {
     it("GET /locations should work without API key", async () => {
-      await request(app.getHttpServer()).get("/location").expect(200);
+      await request(app.getHttpServer()).get("/locations").expect(200);
     });
 
     it("GET /locations/:locationId should work without API key", async () => {
-      await request(app.getHttpServer()).get("/location/1").expect(200);
+      await request(app.getHttpServer()).get("/locations/1").expect(200);
     });
 
     it("POST /locations should fail without API key", async () => {
-      await request(app.getHttpServer()).post("/location").expect(401);
+      await request(app.getHttpServer()).post("/locations").expect(401);
     });
 
     it("PATCH /locations should fail without API key", async () => {
-      await request(app.getHttpServer()).patch("/location").expect(401);
+      await request(app.getHttpServer()).patch("/locations").expect(401);
     });
 
     it("DELETE /locations/:locationId should fail without API key", async () => {
-      await request(app.getHttpServer()).delete("/location/1").expect(401);
+      await request(app.getHttpServer()).delete("/locations/1").expect(401);
     });
   });
 
