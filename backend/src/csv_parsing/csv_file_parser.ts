e0f2aa84092f14d60f79d0dd074535e170e13c21
landgeet <lander.degeeter@ugent.be>
2026-03-03 17:46:42 +0100

    remove hall and fix tests

--- backend/src/csv_parsing/csv_file_parser.ts
+++ backend/src/csv_parsing/csv_file_parser.ts
@@ -91,7 +91,6 @@ export class CSVFileParser {
     return {
       starttime: starttimeDate.toISOString(),
       endtime: endTime,
-      hall: row.Hall,
       production_id: productionId,
       price: row.Price ? Number(row.Price) : null,
     };
