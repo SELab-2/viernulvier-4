35df6bae708a60ab42b5cdd6388ed7a0f3f93b3d
Sebastien Harris <Sebastienmarc.harris@gmail.com>
2026-03-04 23:31:56 +0100

    fix backend path bug

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -6,12 +6,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-
-  // server stuff -> no touchy.
   @Redirect()
   redirectToDocs() {
     const isProduction = process.env.NODE_ENV === "production";
     return { url: isProduction ? "/api/docs" : "/docs", statusCode: 301 };
   }
+
   root(): void {}
 }
