b4e930a2fa68a6f2b22ae3da2cf602f984505d15
Sebastien Harris <Sebastienmarc.harris@gmail.com>
2026-03-04 22:46:12 +0100

    add threat

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// don't touch this without asking @Seb first. baseURL and apiBase must not be altered without knowing what you are doing.
+// If you alter anything and you break Nuxt on the server then I will make you clean up the mess.
 export default defineNuxtConfig({
   ssr: false, // Disable Server-Side Rendering since we'll have a separate backend.
   compatibilityDate: "2025-07-15",
@@ -8,11 +10,10 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: "http://localhost:3000",
+      apiBase: process.env.NUXT_API_BASE || "http://localhost:3000",
     },
   },
   app: {
-    // don't touch this without asking @Seb first. baseURL must not be altered without knowing what you are doing.
     baseURL: process.env.NUXT_BASE || "/",
   },
 });
