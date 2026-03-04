9e540971eabc99c93022d611536092931df9e15c
Sebastien Harris <Sebastienmarc.harris@gmail.com>
2026-03-04 14:06:35 +0100

    please work

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false, // Disable Server-Side Rendering since we'll have a separate backend.
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      apiBase: "http://localhost:3000"
-    }
-  }
-})
+      apiBase: "http://localhost:3000",
+    },
+  },
+  app: {
+    // don't touch this without asking @Seb first. baseURL must not be altered without knowing what you are doing.
+    baseURL: process.env.NUXT_BASE || "/",
+  },
+});
