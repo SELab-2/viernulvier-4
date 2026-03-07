b9e314a921daf9e1f4dd1a697de70d26ee87d96c
Alexander Nollet <alexanderacj13@gmail.com>
2026-03-07 21:06:21 +0100

    Added and tested db connection in the main fucntion for scraper

--- backend/src/tasks/scraper/main.ts
+++ backend/src/tasks/scraper/main.ts
@@ -1,5 +1,7 @@
+import { Production } from "@repo/common";
 import { scrapeMany } from "./scraper";
 import { parseProductions, vnvProduction } from "./vnv.parser";
+import { Pool, QueryResultRow } from "pg";
 
 /**
  * Scrapes the existing data we need from the VNV API.
@@ -19,4 +21,42 @@ async function scrape(
   return vnvProductions;
 }
 
-void scrape("2026-03-04T09:36:21+00:00");
+/**
+ * Execute a query to the database using a pool.
+ * @param pool The pool/connection to the database.
+ * @param query The query that should be used.
+ * @param params The parameters of the query.
+ * @returns The result of the query.
+ */
+async function query<T extends QueryResultRow = any>(
+  pool: Pool,
+  query: string,
+  params?: any[],
+): Promise<T[]> {
+  const res = await pool.query<T>(query, params); // keep await.
+  return res.rows;
+}
+
+/**
+ * Main entrypoint of the worker.
+ */
+async function main() {
+  const pool: Pool = new Pool({
+    user: process.env.DB_USER_DEV,
+    host: process.env.DB_HOST_DEV,
+    database: process.env.DB_NAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    port: Number(process.env.DB_PORT_DEV),
+  });
+
+  const productions: Production[] = await query<Production>(
+    pool,
+    "SELECT * FROM productions",
+    [],
+  );
+
+  console.log(productions);
+  await scrape("2026-03-04T09:36:21+00:00");
+}
+
+void main();
