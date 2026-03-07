d64f3ceea3555b840f73868d6924a041f342d368
Alexander Nollet <alexanderacj13@gmail.com>
2026-03-07 15:50:32 +0100

    The scraper does exactly what it's supposed to do

--- backend/src/tasks/scraper.ts
+++ backend/src/tasks/scraper.ts
@@ -1,34 +1,68 @@
 import "dotenv/config";
+import { URLSearchParams } from "url";
 
-async function fetchProductions(): Promise<object[]> {
-  const url = "https://www.viernulvier.gent/api/v1/productions";
+const apiBase: string = "https://www.viernulvier.gent";
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+interface apiResponse {
+  view: viewState;
+  member: object[];
+}
+
+interface viewState {
+  next: string | null;
+}
+
+async function scrapeUrl(
+  url: string,
+  updatedAfter: string = "1970-01-01T00:00:00+00:00",
+): Promise<object[]> {
+  console.log(
+    `Scraping objects from ${url} with last update time after ${updatedAfter}.`,
+  );
+
   const apiKey = process.env.CLIENT_API_KEY;
-
   if (!apiKey) {
     console.error("Make sure to set CLIENT_API_KEY in .env!");
     return [];
   }
 
-  try {
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        accept: "application/ld+json",
-        "X-AUTH-TOKEN": `${apiKey}`,
-      },
-    });
+  const params = new URLSearchParams();
+  params.append("updated_at[after]", updatedAfter);
+  let view: viewState = {
+    next: url + "&" + params.toString(),
+  };
+  const output: object[] = [];
 
-    if (!response.ok) {
-      throw new Error(`HTTP Error! Status: ${response.status}`);
+  while (view.next) {
+    try {
+      const target: string = apiBase + view.next;
+      console.log(`Fetching ${target}`);
+      const response = await fetch(target, {
+        method: "GET",
+        headers: {
+          accept: "application/ld+json",
+          "X-AUTH-TOKEN": `${apiKey}`,
+        },
+      });
+
+      if (!response.ok)
+        throw new Error(`HTTP Error! Status: ${response.status}`);
+
+      const jsonResponse: apiResponse = (await response.json()) as apiResponse;
+      view = jsonResponse.view;
+
+      output.push(...jsonResponse.member);
+      await delay(1000);
+    } catch (error) {
+      console.error("An error occurred: ", error);
+      console.log("Retrying...");
+      await delay(5000); // Wait a bit longer before retrying.
     }
-
-    console.log(await response.json());
-  } catch (error) {
-    console.error("Failed to fetch data:", error);
-    return [];
   }
 
-  return [];
+  console.log(output.length);
+  return output;
 }
 
-fetchProductions().then();
+scrapeUrl("/api/v1/productions?page=1").then();
