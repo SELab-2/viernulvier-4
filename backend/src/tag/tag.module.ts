3697980609cb4d1534338494343bac504c0bb255
Alexander Nollet <alexanderacj13@gmail.com>
2026-03-10 12:44:34 +0100

    Tags updated with Language support

--- backend/src/tag/tag.module.ts
+++ backend/src/tag/tag.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { TagService } from "./tag.service";
 import { TagController } from "./tag.controller";
 import { DbModule } from "../database/db.module";
+import { UtilModule } from "src/util/util.module";
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, UtilModule],
   controllers: [TagController],
   providers: [TagService],
 })
