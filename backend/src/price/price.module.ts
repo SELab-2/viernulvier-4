2a2613562a8e4bc3defd86a178422be88bbae9bc
Alexander Nollet <alexanderacj13@gmail.com>
2026-03-10 12:38:21 +0100

    Price updated to language support

--- backend/src/price/price.module.ts
+++ backend/src/price/price.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { PriceService } from "./price.service";
 import { PriceController } from "./price.controller";
 import { DbModule } from "../database/db.module";
+import { UtilModule } from "src/util/util.module";
 
 @Module({
   providers: [PriceService],
   controllers: [PriceController],
-  imports: [DbModule],
+  imports: [DbModule, UtilModule],
 })
 export class PriceModule {}
