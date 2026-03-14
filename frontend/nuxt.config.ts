42d9fde09f33d4cc609620795ca0d9b33b9107c8
Robin De Rudder <robin.derudder@ugent.be>
2026-03-14 20:28:09 +0100

    improved "discover more" section + fixed favicon ?

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
     head: {
       title: 'Viernulvier-Archive', 
       link: [
-        { rel: 'icon', type: 'image/png', href: '/blue_black_reg.png' }
+        { rel: 'icon', type: 'image/png', href: '~/public/blue_black_reg.png' }
       ]
     }
   },
