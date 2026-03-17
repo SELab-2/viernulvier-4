3e640b4d88afcba2f557fc4e4dbb01722c0add45
Alexander Nollet <alexanderacj13@gmail.com>
2026-03-17 15:41:56 +0100

    Generic paginated object

--- common/src/objects/extra.ts
+++ common/src/objects/extra.ts
@@ -4,14 +4,13 @@ export const PaginationFilterSchema = z.object({
   page: z.coerce.number().min(0).default(0),
   limit: z.coerce.number().min(0).max(100).default(20),
 });
-
-export const PaginatedResponseSchema = <T extends z.ZodTypeAny>(schema: T) =>
-  z.object({
-    page: z.coerce.number().min(0).default(0),
-    limit: z.coerce.number().min(0).max(100).default(20),
-    totalItems: z.coerce.number(),
-    objects: z.array(schema),
-  });
-
 export type PaginationFilter = z.infer<typeof PaginationFilterSchema>;
-export type PaginatedResponse = z.infer<typeof PaginatedResponseSchema>;
+
+// Paginated Response is a non ZOD type because of it's nature of only being
+// returned by the API.
+export type PaginatedResponse<T> = {
+  page: number;
+  limit: number;
+  totalItems: number;
+  objects: T[];
+};
