623108d2c58074481b79e043cc779b867ed7fd0d
Diana Dendauw <Diana.Dendauw@UGent.be>
2026-03-17 12:28:52 +0100

    restoring tailwind

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,6 +6,17 @@ export default defineNuxtConfig({
   ssr: false, // Disable Server-Side Rendering since we'll have a separate backend.
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  modules: ['@nuxtjs/i18n'],
+  i18n: {
+    locales: [
+      { code: 'nl', language: 'nl-BE', file: 'nl.json', name: 'Nederlands' },
+      { code: 'en', language: 'en-GB', file: 'en.json', name: 'English' },
+    ],
+    defaultLocale: 'nl',
+    langDir: 'locales/',
+    restructureDir: '',
+    strategy: 'no_prefix', // no /nl/ or /en/ in URL
+  },
   devServer: {
     port: 3001,
   },
