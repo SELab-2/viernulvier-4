511630c459db8ed11ac70b2b0e85073e250bfe98
Jill Van Ham <Jill.VanHam@UGent.be>
2026-03-21 16:51:38 +0100

    default header

--- frontend/app/components/AppHeader.vue
+++ frontend/app/components/AppHeader.vue
@@ -7,7 +7,7 @@ import logoBlack from '~/assets/logo_black.svg'
 import logoWhite from '~/assets/logo_white.svg'
 
 const { isLoggedIn, logout } = useAuth()
-const isAdmin = ref(true) // isLoggedIn
+const isAdmin = isLoggedIn // to test admin: const isAdmin = ref(true)
 
 const { t, locale, setLocale } = useI18n()
 const isDark = ref(false)
