e54222d7a84d9a64f4d42f573ec1561b1f35bfe3
Alexander Nollet <alexanderacj13@gmail.com>
2026-03-22 17:38:27 +0100

    Updated the controller for media galleries with all needed suppor

--- backend/src/media/controllers/media.gallery.controller.ts
+++ backend/src/media/controllers/media.gallery.controller.ts
@@ -1,10 +1,39 @@
-import { Controller, Get, Query } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from "@nestjs/common";
 import { MediaGalleryService } from "../services/media.gallery.service";
-import { PaginatedResponse, PaginationFilterSchema } from "@repo/common";
-import { MediaGalleryDto, PaginationFilterDto } from "../../dto/dto";
+import {
+  CreateMediaGallerySchema,
+  PaginatedResponse,
+  PaginationFilterSchema,
+  UpdatedMediaGallerySchema,
+} from "@repo/common";
+import {
+  CreateMediaGalleryDto,
+  MediaGalleryDto,
+  MediaItemDto,
+  PaginationFilterDto,
+  UpdateMediaGalleryDto,
+} from "../../dto/dto";
 import { ZodValidationPipe } from "nestjs-zod";
-import { ApiOperation } from "@nestjs/swagger";
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiSecurity,
+} from "@nestjs/swagger";
 import { ApiOkPaginatedResponseAnyOf } from "../../common/decorators/api.ok";
+import { ApiKeyGuard } from "../../auth/authGuard";
 
 /**
  * Defines all media gallery related endpoints.
@@ -26,5 +55,138 @@ export class MediaGalleryController {
     paginationFilter: PaginationFilterDto,
   ): Promise<PaginatedResponse<MediaGalleryDto>> {
     return await this.mediaGalleryService.getGalleries(paginationFilter);
+  }
+
+  /**
+   * Responds to a GET to "/media/galleries/:galleryId"
+   * @param galleryId The ID of the gallery in the URL.
+   * @returns The found gallery.
+   */
+  @ApiOperation({ summary: "Fetches one single media gallery." })
+  @ApiOkResponse({ type: MediaGalleryDto, description: "Found gallery." })
+  @Get(":galleryId")
+  async getGalleryById(
+    @Param("galleryId", ParseIntPipe) galleryId: number,
+  ): Promise<MediaGalleryDto> {
+    return await this.mediaGalleryService.getGalleryById(galleryId);
+  }
+
+  /**
+   * Responds to a POST to "/media/galleries"
+   * @param createGallery The object we want to create.
+   * @returns The newly created gallery object.
+   */
+  @UseGuards(ApiKeyGuard)
+  @ApiSecurity("apiKey")
+  @ApiOperation({ summary: "Creates a new media gallery." })
+  @ApiCreatedResponse({
+    type: MediaGalleryDto,
+    description: "Created gallery.",
+  })
+  @Post()
+  async createGallery(
+    @Body(new ZodValidationPipe(CreateMediaGallerySchema))
+    createGallery: CreateMediaGalleryDto,
+  ): Promise<MediaGalleryDto> {
+    return await this.mediaGalleryService.createGallery(createGallery);
+  }
+
+  /**
+   * Responds to a PATCH to "/media/galleries/:galleryId"
+   * @param galleryId The ID of the gallery.
+   * @param modifyGallery The fields needed to modify it.
+   * @returns The newly modified gallery.
+   */
+  @UseGuards(ApiKeyGuard)
+  @ApiSecurity("apiKey")
+  @ApiOperation({ summary: "Modifies an existing gallery." })
+  @ApiOkResponse({
+    type: MediaGalleryDto,
+    description: "Modified an existing gallery.",
+  })
+  @Patch(":galleryId")
+  async modifyGallery(
+    @Param("galleryId", ParseIntPipe) galleryId: number,
+    @Body(new ZodValidationPipe(UpdatedMediaGallerySchema))
+    modifyGallery: UpdateMediaGalleryDto,
+  ): Promise<MediaGalleryDto> {
+    return await this.mediaGalleryService.modifyGallery(
+      galleryId,
+      modifyGallery,
+    );
+  }
+
+  /**
+   * Responds to a DELETE to "/media/galleries/:galleryId"
+   * @param galleryId The ID of the gallery we want to delete.
+   */
+  @UseGuards(ApiKeyGuard)
+  @ApiSecurity("apiKey")
+  @ApiOperation({ summary: "Deletes an existing gallery." })
+  @ApiOkResponse({
+    description: "Deleted the existing gallery.",
+  })
+  @Delete(":galleryId")
+  async deleteGallery(
+    @Param("galleryId", ParseIntPipe) galleryId: number,
+  ): Promise<void> {
+    await this.mediaGalleryService.deleteGallery(galleryId);
+  }
+
+  /**
+   * Gallery -- Item
+   */
+
+  /**
+   * Responds to a GET to "/media/galleries/:galleryId/items"
+   * @param galleryId The ID of the gallery in the URL.
+   * @returns A list of items for that gallery.
+   */
+  @ApiOperation({ summary: "Gets the items from one gallery." })
+  @ApiOkResponse({
+    type: MediaItemDto,
+    isArray: true,
+    description: "Fetched items from gallery",
+  })
+  @Get(":galleryId/items")
+  async getGalleryItems(
+    @Param("galleryId", ParseIntPipe) galleryId: number,
+  ): Promise<MediaItemDto[]> {
+    return await this.mediaGalleryService.getGalleryItems(galleryId);
+  }
+
+  /**
+   * Responds to a PUT to "/media/galleries/:galleryId/items/:itemId"
+   * @param galleryId The ID of the gallery.
+   * @param itemId The ID of the item.
+   * @returns T/F Whether the link was made or not.
+   */
+  @UseGuards(ApiKeyGuard)
+  @ApiSecurity("apiKey")
+  @ApiOperation({ summary: "Links a media item to a media gallery." })
+  @ApiOkResponse({ description: "Item linked." })
+  @Put(":galleryId/items/:itemId")
+  async linkItemToGallery(
+    @Param("galleryId", ParseIntPipe) galleryId: number,
+    @Param("itemId", ParseIntPipe) itemId: number,
+  ): Promise<boolean> {
+    return await this.linkItemToGallery(galleryId, itemId);
+  }
+
+  /**
+   * Responds to a DELETE to "/media/galleries/:galleryId/items/:itemId"
+   * @param galleryId The ID of the gallery.
+   * @param itemId The ID of the item.
+   */
+  @UseGuards(ApiKeyGuard)
+  @ApiSecurity("apiKey")
+  @ApiOperation({ summary: "Unlinks a media item from a media gallery." })
+  @ApiOkResponse({ description: "Item unlinked." })
+  @Delete(":galleryId/items/:itemId")
+  async unlinkItemFromGallery(
+    @Param("galleryId", ParseIntPipe) galleryId: number,
+    @Param("itemId", ParseIntPipe) itemId: number,
+  ): Promise<void> {
+    await this.unlinkItemFromGallery(galleryId, itemId);
   }
 }
