29c74a0532ac822b49754dcb10d1b8eca0029b5e
landgeet <lander.degeeter@ugent.be>
2026-03-23 23:48:18 +0100

    fix dbmodule missing

--- backend/src/parser/parser.module.ts
+++ backend/src/parser/parser.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { ParserController } from "./parser.controller";
 import { ScraperModule } from "../util/scraper/scraper.module";
+import { DbModule } from "../database/db.module";
 
 @Module({
   controllers: [ParserController],
-  imports: [ScraperModule],
+  imports: [ScraperModule, DbModule],
 })
 export class ParserModule {}
