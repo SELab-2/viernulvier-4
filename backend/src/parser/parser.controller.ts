60299efa2d755559cb8b8aec483f792e24a6cb3a
landgeet <lander.degeeter@ugent.be>
2026-03-23 23:52:59 +0100

    add ok responses

--- backend/src/parser/parser.controller.ts
+++ backend/src/parser/parser.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body, UseGuards } from "@nestjs/common";
 import { CsvInjectionService } from "../util/scraper/csv-injection.service";
-import { ApiOperation, ApiSecurity, ApiTags } from "@nestjs/swagger";
+import { ApiOkResponse, ApiOperation, ApiSecurity} from "@nestjs/swagger";
 import { ApiKeyGuard } from "src/auth/authGuard";
 
 @Controller("parser")
@@ -10,6 +10,7 @@ export class ParserController {
   @UseGuards(ApiKeyGuard)
   @ApiSecurity("api_key")
   @ApiOperation({ summary: "Inject productions from CSV file" })
+  @ApiOkResponse({ description: "Productions CSV injected successfully" })
   @Post("productions")
   async injectProductions(@Body("filePath") filePath: string) {
     return this.csvInjectionService.injectProductionsCSV(filePath);
@@ -18,6 +19,7 @@ export class ParserController {
   @UseGuards(ApiKeyGuard)
   @ApiSecurity("api_key")
   @ApiOperation({ summary: "Inject events from CSV file" })
+  @ApiOkResponse({ description: "Events CSV injected successfully" })
   @Post("events")
   async injectEvents(@Body("filePath") filePath: string) {
     return this.csvInjectionService.injectEventsCSV(filePath);
@@ -26,6 +28,7 @@ export class ParserController {
   @UseGuards(ApiKeyGuard)
   @ApiSecurity("api_key")
   @ApiOperation({ summary: "Inject tags from CSV file" })
+  @ApiOkResponse({ description: "Tags CSV injected successfully" })
   @Post("tags")
   async injectTags(@Body("filePath") filePath: string) {
     return this.csvInjectionService.injectTagsCSV(filePath);
@@ -34,6 +37,7 @@ export class ParserController {
   @UseGuards(ApiKeyGuard)
   @ApiSecurity("api_key")
   @ApiOperation({ summary: "Inject blogs from CSV file" })
+  @ApiOkResponse({ description: "Blogs CSV injected successfully" })
   @Post("blogs")
   async injectBlogs(@Body("filePath") filePath: string) {
     return this.csvInjectionService.injectBlogsCSV(filePath);
@@ -42,6 +46,7 @@ export class ParserController {
   @UseGuards(ApiKeyGuard)
   @ApiSecurity("api_key")
   @ApiOperation({ summary: "Inject prices from CSV file" })
+  @ApiOkResponse({ description: "Prices CSV injected successfully" })
   @Post("prices")
   async injectPrices(@Body("filePath") filePath: string) {
     return this.csvInjectionService.injectPricesCSV(filePath);
