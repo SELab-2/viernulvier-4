3154a5fd31a8938878e4693d6787b5161924335a
landgeet <lander.degeeter@ugent.be>
2026-03-23 23:34:15 +0100

    implement basic controller

--- backend/src/parser/parser.controller.ts
+++ backend/src/parser/parser.controller.ts
@@ -1,8 +1,32 @@
-import { Controller } from "@nestjs/common";
-
+import { Controller, Post, Body } from "@nestjs/common";
 import { CsvInjectionService } from "../util/scraper/csv-injection.service";
 
 @Controller("parser")
 export class ParserController {
   constructor(private readonly csvInjectionService: CsvInjectionService) {}
+
+  @Post("productions")
+  async injectProductions(@Body("filePath") filePath: string) {
+    return this.csvInjectionService.injectProductionsCSV(filePath);
+  }
+
+  @Post("events")
+  async injectEvents(@Body("filePath") filePath: string) {
+    return this.csvInjectionService.injectEventsCSV(filePath);
+  }
+
+  @Post("tags")
+  async injectTags(@Body("filePath") filePath: string) {
+    return this.csvInjectionService.injectTagsCSV(filePath);
+  }
+
+  @Post("blogs")
+  async injectBlogs(@Body("filePath") filePath: string) {
+    return this.csvInjectionService.injectBlogsCSV(filePath);
+  }
+
+  @Post("prices")
+  async injectPrices(@Body("filePath") filePath: string) {
+    return this.csvInjectionService.injectPricesCSV(filePath);
+  }
 }
