4ab1cfe8c909e1bd7f81e47e4c4d0271bfb11479
Diana Dendauw <Diana.Dendauw@UGent.be>
2026-03-29 15:19:40 +0200

    added showcase

---
+++ frontend/app/pages/TempShowcase.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { StoryItem } from '../types/StoryItem'
+
+const stories: StoryItem[] = [
+  {
+    id: '1',
+    title: 'TITEL 1',
+    date: new Date(2025, 2, 15),
+    description: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+  },
+  {
+    id: '2',
+    title: 'TITEL 2',
+    date: new Date(2025, 3, 20),
+    description: 'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBb',
+  },
+  {
+    id: '3',
+    title: 'TITEL 3',
+    date: new Date(2025, 5, 1),
+    description: 'CCCCCCCCCCCCCCCCCCCCCCCCCCCCCC',
+  }
+]
+</script>
+
+<template>
+  <ProductionStoryListView :stories="stories" />
+</template>
+
+<style scoped>
+
+</style>
