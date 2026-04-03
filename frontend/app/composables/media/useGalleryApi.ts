0ae2fbe469d9688990eb7e0269e81a1cb21118ef
Alexander Nollet <alexanderacj13@gmail.com>
2026-04-03 22:47:51 +0200

    Full gallery useAPI

--- frontend/app/composables/media/useGalleryApi.ts
+++ frontend/app/composables/media/useGalleryApi.ts
@@ -1,12 +1,19 @@
 import type {
+  CreateMediaGallery,
   MediaGallery,
+  MediaItem,
+  ModifyMediaGallery,
   PaginatedResponse,
   PaginationFilter,
+  ReplaceMediaGallery,
 } from "@repo/common";
 
 export function useGalleryApi() {
   const { get, post, patch, put, del } = useApi();
 
+  /**
+   * Get all media galleries.
+   */
   const getAll = (paginationFilters: PaginationFilter) => {
     const params = {
       ...paginationFilters,
@@ -28,5 +35,73 @@ export function useGalleryApi() {
     );
   };
 
-  return { getAll };
+  /**
+   * Get a specific gallery by it's ID.
+   */
+  const getById = (galleryId: number) =>
+    get<MediaGallery>(API_ROUTES.galleries.byId(galleryId));
+
+  /**
+   * Create a new gallery.
+   */
+  const create = (body: CreateMediaGallery) =>
+    post<MediaGallery, CreateMediaGallery>(API_ROUTES.galleries.base, body);
+
+  /**
+   * Replace a gallery.
+   */
+  const replace = (galleryId: number, body: ReplaceMediaGallery) =>
+    put<MediaGallery, ReplaceMediaGallery>(
+      API_ROUTES.galleries.byId(galleryId),
+      body,
+    );
+
+  /**
+   * Modifies an existing gallery.
+   */
+  const modify = (galleryId: number, body: ModifyMediaGallery) =>
+    patch<MediaGallery, ModifyMediaGallery>(
+      API_ROUTES.galleries.byId(galleryId),
+      body,
+    );
+
+  /**
+   * Removes an existing gallery.
+   */
+  const remove = (galleryId: number) =>
+    del(API_ROUTES.galleries.byId(galleryId));
+
+  /**
+   * -- Item links.
+   */
+
+  /**
+   * Returns all items linked to a gallery.
+   */
+  const getGalleryItems = (galleryId: number) =>
+    get<MediaItem[]>(API_ROUTES.galleries.items(galleryId));
+
+  /**
+   * Links an item to a gallery.
+   */
+  const linkItemToGallery = (galleryId: number, itemId: number) =>
+    put(API_ROUTES.galleries.itemLink(galleryId, itemId), {});
+
+  /**
+   * Unlinks an item from a gallery.
+   */
+  const unlinkItemFromGallery = (galleryId: number, itemId: number) =>
+    del(API_ROUTES.galleries.itemLink(galleryId, itemId));
+
+  return {
+    getAll,
+    getById,
+    create,
+    replace,
+    modify,
+    remove,
+    getGalleryItems,
+    linkItemToGallery,
+    unlinkItemFromGallery,
+  };
 }
