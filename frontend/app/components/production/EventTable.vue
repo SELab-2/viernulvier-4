64fc73699f0a4294fff8560b299f7689837dd5fb
Diana Dendauw <Diana.Dendauw@UGent.be>
2026-04-04 15:56:59 +0200

    updated documentation a little bit

--- frontend/app/components/production/EventTable.vue
+++ frontend/app/components/production/EventTable.vue
@@ -14,6 +14,8 @@
  *    { id: 1, starttime: '2026-03-10T19:30:00Z', endtime: null, doors_at: null, intermission_at: null, created_at: '2026-03-10T00:00:00Z', updated_at: '2026-03-10T00:00:00Z', production_id: 1, locations: [{ id: 1, location: 'Antwerpen', created_at: '2026-03-10T00:00:00Z', updated_at: '2026-03-10T00:00:00Z' }], prices: [{ id: 1, price: 15.00, name: 'Volwassenen', created_at: '2026-03-10T00:00:00Z', updated_at: '2026-03-10T00:00:00Z' }] },
  *    { id: 2, starttime: '2026-03-29T19:30:00Z', endtime: null, doors_at: null, intermission_at: null, created_at: '2026-03-29T00:00:00Z', updated_at: '2026-03-29T00:00:00Z', production_id: 1, locations: [{ id: 2, location: 'Gent', created_at: '2026-03-29T00:00:00Z', updated_at: '2026-03-29T00:00:00Z' }], prices: [{ id: 2, price: 12.50, name: 'Volwassenen', created_at: '2026-03-29T00:00:00Z', updated_at: '2026-03-29T00:00:00Z' }] }
  * ]
+ *
+ * For multiple locations and/or prices, just add to the list.
  */
 
 import type { EventWithDetails } from "../../types/EventWithDetails";
