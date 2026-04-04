41bd3212a4a82c76aef5515c102da889f8a35f75
Diana Dendauw <Diana.Dendauw@UGent.be>
2026-04-04 22:34:40 +0200

    forgot to update documentation

--- frontend/app/components/production/EventTable.vue
+++ frontend/app/components/production/EventTable.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">/**
  * A reusable event table component, displays events belonging to a specific production in a table, includes:
  *  - Time, location, price displayed per event
- *  - Sorting of events (oldest first)
  *  - Scrollable when there are more than 3 events
  *
  * Usage:
