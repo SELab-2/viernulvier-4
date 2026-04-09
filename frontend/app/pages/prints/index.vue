85f10561d09a3be86da04468f8ce274064e8bc45
Diana Dendauw <Diana.Dendauw@UGent.be>
2026-04-09 09:20:04 +0200

    added header

--- frontend/app/pages/prints/index.vue
+++ frontend/app/pages/prints/index.vue
@@ -3,7 +3,11 @@
 </script>
 
 <template>
+  <div class="min-h-screen bg-white dark:bg-[#151821] text-gray-900 dark:text-gray-100 transition-colors duration-200">
 
+    <PrintsHeader/>
+
+  </div>
 </template>
 
 <style scoped>
