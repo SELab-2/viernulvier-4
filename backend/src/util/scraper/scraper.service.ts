b64a9cf968f3dd8056a30a7a05b227f0d4a60c6f
Sebastien Harris <Sebastienmarc.harris@gmail.com>
2026-04-09 14:43:57 +0200

    try headers

--- backend/src/util/scraper/scraper.service.ts
+++ backend/src/util/scraper/scraper.service.ts
@@ -210,6 +210,13 @@ export class ScraperService implements OnApplicationBootstrap {
     const response = await fetch(url, {
       // @ts-ignore - dispatcher is a Node-specific extension to standard fetch
       dispatcher: ipv4Agent,
+
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        Accept:
+          "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+      },
     });
 
     if (!response.ok) {
