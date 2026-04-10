96812b2eb9523bee445ffdd796003a01c09314ef
Alexander Nollet <alexanderacj13@gmail.com>
2026-04-10 12:10:10 +0200

    Update backend/src/database/db.account.service.ts
    Co-authored-by: Lander De Geeter <147646023+landgeet@users.noreply.github.com>

--- backend/src/database/db.account.service.ts
+++ backend/src/database/db.account.service.ts
@@ -92,7 +92,7 @@ export class AccountDatabaseService {
       LIMIT 1
     `;
 
-    // Returning AccountDto should be safe here since we never show this to the user.s
+    // Returning AccountDto should be safe here since we never show this to the users.
     const result = await this.db.query<AccountDto>(query, [account.username]);
 
     if (result.length === 0) {
