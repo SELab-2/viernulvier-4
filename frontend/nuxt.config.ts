32d4cb4cb4daa879d44e52ef79b33b4d7119b5ad
Alexander Nollet <alexanderacj13@gmail.com>
2026-04-13 01:15:58 +0200

    Updated worker to delete nuxt cache when deploying

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,9 +1,3 @@
-// import dotenv from 'dotenv';
-// import path from 'path';
-// import process from 'process';
-
-// dotenv.config({ path: path.resolve(process.cwd(), "../.env"), quiet: true });
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // don't touch this without asking @Seb first. baseURL and apiBase must not be altered without knowing what you are doing.
 // If you alter anything and you break Nuxt on the server then I will make you clean up the mess.
