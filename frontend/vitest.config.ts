73fe489c7394e7aadcf68d95baace062d4ac5d11
Alexander Nollet <alexanderacj13@gmail.com>
2026-04-14 19:56:46 +0200

    FIXED THE VERBOSE OUTPUT DURING FRONTEND TESTS YIPPEEE

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -9,20 +9,7 @@ export default defineVitestConfig({
       },
     },
     globals: true,
-
-    onConsoleLog(log) {
-      if (log.includes("<Suspense> is an experimental feature")) return false;
-
-      if (log.includes("No match found for location with path")) return false;
-      if (log.includes('Importing from "vitest/environments" is deprecated'))
-        return false;
-
-      if (log.includes("[useApi] Network error")) return false;
-      if (log.includes("[useApi] Not Found")) return false;
-      if (log.includes("fetch failed")) return false;
-      if (log.includes("handleDefaultError")) return false;
-    },
-
+    silent: true,
     include: ["tests/**/*.spec.ts", "app/**/*.spec.ts"],
     coverage: {
       provider: "v8",
