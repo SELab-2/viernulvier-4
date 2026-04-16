917bcc0170f8c111d13379204a11d0d7a1afc1f9
Diana Dendauw <Diana.Dendauw@UGent.be>
2026-04-16 20:54:17 +0200

    fixed files not opening anymore

--- frontend/app/components/prints/FileGridItem.vue
+++ frontend/app/components/prints/FileGridItem.vue
@@ -28,6 +28,7 @@ const openFile = (src: string) => window.open(src, "_blank"); // for opening the
     <!-- Thumbnail -->
     <div
       class="relative w-full rounded-lg overflow-hidden border border-border aspect-[3/4] group-hover:border-accent/60 transition-colors duration-150"
+      @click="openFile(file.url)"
     >
       <MediaDisplay :src="file" size="fill" :show-icon="true" />
     </div>
