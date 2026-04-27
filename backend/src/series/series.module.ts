69db9068bff4f56f1b012877a656b7e81d99e13b
Alexander Nollet <alexanderacj13@gmail.com>
2026-04-27 16:04:54 +0200

    controller for series

--- backend/src/series/series.module.ts
+++ backend/src/series/series.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import { SeriesService } from "./series.service";
 import { SeriesController } from "./series.controller";
+import { DbModule } from "../database/db.module";
+import { LanguageModule } from "../util/language/LanguageModule";
 
 @Module({
+  imports: [DbModule, LanguageModule],
   providers: [SeriesService],
   controllers: [SeriesController],
 })
