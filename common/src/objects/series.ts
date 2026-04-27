1c51e08f9afe01a7bd9c014868d7126d1ded759f
Sebastien Harris <Sebastienmarc.harris@gmail.com>
2026-04-27 14:53:30 +0200

    add series db service

---
+++ common/src/objects/series.ts
@@ -0,0 +1,38 @@
+import z from "zod";
+import { LocalizedStringSchema } from "./language";
+import { PriceSchema } from "./prices";
+
+// Base Series object.
+export const SeriesSchema = z.object({
+  id: z.number(),
+  titel: LocalizedStringSchema,
+  desc: LocalizedStringSchema,
+  created_at: z.iso.datetime(),
+  updated_at: z.iso.datetime(),
+  // Legacy ID is omitted here because the API doesn't use it.
+});
+
+// Localized Series object.
+export const SeriesViewSchema = PriceSchema.extend({
+  titel: z.string(),
+  desc: z.string(),
+});
+
+// Omits read-only fields.
+const MutableSeriesSchema = PriceSchema.omit({
+  id: true,
+  created_at: true,
+  updated_at: true,
+});
+
+// Updating & Creating.
+export const CreateSeriesSchema = MutableSeriesSchema;
+export const ModifySeriesSchema = MutableSeriesSchema.partial();
+export const ReplaceSeriesSchema = MutableSeriesSchema;
+
+// Type exports.
+export type Series = z.infer<typeof SeriesSchema>;
+export type SeriesView = z.infer<typeof SeriesViewSchema>;
+export type CreateSeries = z.infer<typeof CreateSeriesSchema>;
+export type ModifySeries = z.infer<typeof ModifySeriesSchema>;
+export type ReplaceSeries = z.infer<typeof ReplaceSeriesSchema>;
