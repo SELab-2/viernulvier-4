41049594738a710f2804ae42df75a7739cab3750
Dominik <dominik.nowak2003@gmail.com>
2026-04-28 22:52:20 +0200

    production form created, added create and edit page but just basics

---
+++ frontend/app/components/admin/productions/Form.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import type { FormField } from "~/types/FormField";
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+
+const emit = defineEmits<{
+  submit: [Record<string, any>];
+}>();
+
+const fields = computed<FormField[]>(() => [
+  {
+    component: "BaseInput",
+    name: "titel",
+    props: {
+      label: t("admin-productions.form-title"),
+      required: true,
+    },
+  },
+  {
+    component: "BaseTextArea",
+    name: "description1",
+    props: {
+      label: t("admin-productions.form-description1"),
+      required: true,
+    },
+  },
+  {
+    component: "BaseTextArea",
+    name: "description2",
+    props: {
+      label: t("admin-productions.form-description2"),
+      required: true,
+    },
+  },
+  {
+    component: "BaseInput",
+    name: "tagline",
+    props: {
+      label: t("admin-productions.form-tagline"),
+    },
+  },
+  {
+    component: "BaseTextArea",
+    name: "credits",
+    props: {
+      label: t("admin-productions.form-credits"),
+    },
+  },
+  {
+    component: "BaseInput",
+    name: "artist",
+    props: {
+      label: t("admin-productions.form-artists"),
+    },
+  },
+]);
+</script>
+
+<template>
+  <FormBaseForm :fields="fields" @submit="emit('submit', $event)" />
+</template>
---
+++ frontend/app/pages/admin/productions/edit/[id].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="p-6">
+    <AdminProductionsForm />
+  </div>
+</template>
+
+<style scoped></style>
