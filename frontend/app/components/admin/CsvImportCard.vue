846a349b1a29eca1ef6b9240b8efc72a3753cf87
landgeet <lander.degeeter@ugent.be>
2026-05-09 14:47:16 +0200

    first basic version

---
+++ frontend/app/components/admin/CsvImportCard.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { ParserTarget } from "~/composables/useParserApi";
+
+const props = defineProps<{ target: ParserTarget }>();
+</script>
+
+<template>
+  <div class="space-y-6">
+    <AdminCsvTemplateDownload :target="props.target" />
+    <AdminCsvUploadComponent :target="props.target" />
+  </div>
+</template>
