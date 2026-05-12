d02afd27d271fcda7c58926eae59f9e7455ffdc0
Diana Dendauw <Diana.Dendauw@UGent.be>
2026-05-12 19:35:06 +0200

    updated temp

--- frontend/app/pages/temp.vue
+++ frontend/app/pages/temp.vue
@@ -6,45 +6,35 @@ import type {
 } from "@repo/common";
 
 const seriesApi = useSeriesApi();
-const { locale } = useI18n();
+const { locale, t } = useI18n();
 
 const SERIES_ID_1 = 2; // Series without productions on dev
 const SERIES_ID_2 = 3; // Series without more productions on dev
 const SERIES_ID_3 = 4; // Series with one production on dev
+const SERIES_ID_4 = 5; // Series with a lot of productions on dev (to check pagination)
 
-const series1 = ref<SeriesView | null>(null);
-const productions1 = ref<ProductionView[]>([]);
-
-const series2 = ref<SeriesView | null>(null);
-const productions2 = ref<ProductionView[]>([]);
-
-const series3 = ref<SeriesView | null>(null);
-const productions3 = ref<ProductionView[]>([]);
+const seriesList = ref<{ series: SeriesView; totalItems: number }[]>([]);
 
 async function loadAll() {
-  const [s1Res, p1Res, s2Res, p2Res, s3Res, p3Res] = await Promise.all([
-    seriesApi.getById(SERIES_ID_1, locale.value),
-    seriesApi.getSeriesProductions(SERIES_ID_1, locale.value),
-    seriesApi.getById(SERIES_ID_2, locale.value),
-    seriesApi.getSeriesProductions(SERIES_ID_2, locale.value),
-    seriesApi.getById(SERIES_ID_3, locale.value),
-    seriesApi.getSeriesProductions(SERIES_ID_3, locale.value),
-  ]);
+  const ids = [SERIES_ID_1, SERIES_ID_2, SERIES_ID_3, SERIES_ID_4];
 
-  if (s1Res.data) series1.value = s1Res.data as SeriesView;
-  if (p1Res.data)
-    productions1.value =
-      (p1Res.data as PaginatedResponse<ProductionView>).objects ?? [];
+  const results = await Promise.all(
+    ids.map((id) =>
+      Promise.all([
+        seriesApi.getById(id, locale.value),
+        seriesApi.getSeriesProductions(id, locale.value),
+      ]),
+    ),
+  );
 
-  if (s2Res.data) series2.value = s2Res.data as SeriesView;
-  if (p2Res.data)
-    productions2.value =
-      (p2Res.data as PaginatedResponse<ProductionView>).objects ?? [];
-
-  if (s3Res.data) series3.value = s3Res.data as SeriesView;
-  if (p3Res.data)
-    productions3.value =
-      (p3Res.data as PaginatedResponse<ProductionView>).objects ?? [];
+  seriesList.value = results
+    .map(([sRes, pRes]) => {
+      const series = sRes.data as SeriesView | null;
+      const paginated = pRes.data as PaginatedResponse<ProductionView>;
+      const totalItems = paginated?.totalItems ?? 0;
+      return series ? { series, totalItems } : null;
+    })
+    .filter(Boolean) as { series: SeriesView; totalItems: number }[];
 }
 
 watch(locale, loadAll);
@@ -53,23 +43,14 @@ onMounted(loadAll);
 
 <template>
   <div class="page-container py-8 flex flex-col gap-8">
-    <SeriesScroller
-      v-if="series1"
-      :series="series1"
-      :productions="productions1"
-    />
-    <br />
-    <SeriesScroller
-      v-if="series2"
-      :series="series2"
-      :productions="productions2"
-    />
-    <br />
-    <SeriesScroller
-      v-if="series3"
-      :series="series3"
-      :productions="productions3"
-    />
+    <div v-if="seriesList.length" class="flex flex-wrap gap-6">
+      <SeriesFolder
+        v-for="item in seriesList"
+        :key="item.series.id"
+        :series="item.series"
+        :production-count="item.totalItems"
+      />
+    </div>
   </div>
 </template>
 
