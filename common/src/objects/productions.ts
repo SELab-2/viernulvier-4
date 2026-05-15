04d22fb11913659cf9911ad6120b980705532750
Alexander Nollet <alexanderacj13@gmail.com>
2026-05-15 21:28:49 +0200

    Extra production filter that let's you get productions for a blog

--- common/src/objects/productions.ts
+++ common/src/objects/productions.ts
@@ -62,6 +62,9 @@ export const FilterProductionSchema = z.object({
   performer_type: z.string().optional(),
   attendance_mode: z.string().optional(),
 
+  // This will return all productions that are tied to the blog.
+  blog_id: z.coerce.number().optional(),
+
   // Toggle for the backend to treat the request as a suggestion.
   is_suggestion: QueryBoolean.default(false),
 });
