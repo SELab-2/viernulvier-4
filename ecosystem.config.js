7e0d601f494b2070dcb99c2d7014e7ea5205f67f
Sebastien Harris <Sebastienmarc.harris@gmail.com>
2026-05-19 13:25:51 +0200

    add db changes to streamline deployment.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       name: "frontend",
       script: ".output/server/index.mjs",
-      cwd: "/home/selab2/project/frontend",
+      cwd: "/path/to/project/frontend",
       env: {
         PORT: 3001,
         NUXT_API_BASE: "https://sel2-4.ugent.be/api",
@@ -14,7 +14,7 @@ module.exports = {
     {
       name: "backend",
       script: "dist/main.js",
-      cwd: "/home/selab2/project/backend",
+      cwd: "/path/to/project/backend",
       env: {
         PORT: 3000,
         NODE_ENV: "production",
